0116bbaab03d80a95c8f8b32fec8d246d7d0e81d
liuhongda <846061407@qq.com>
2018-10-29 14:14:33 +0800

    项目搭建

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<UserInfo> userList() {
+        return userMapper.selectAll();
+    }
+}
