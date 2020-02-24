177d4c940396e1cbb48a7e7278536a2a83ba484a
liuzhuzhu <846061407@qq.com>
2020-02-24 19:08:50 +0800

    Update GitTest.java

--- gmall-api/src/main/java/com/atguigu/gmall/bean/GitTest.java
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/GitTest.java
@@ -8,10 +8,16 @@ public class GitTest {
         System.out.println("开始阿兹1.0.1 开发代码了");
 
         int e = 1;
+        
+        int x = 2;
 
     }
 
     public void wxPay(){
+        System.out.println("微信支付完成...");
+    }
+    
+    public void wxPay(xxxxx){
         System.out.println("微信支付完成...");
     }
 
