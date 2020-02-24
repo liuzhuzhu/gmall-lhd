86ca37f4fe9727c70571cb9d27f5dfc60369b5b0
liuhongda <liuhondga@atguigu.com>
2020-02-24 14:38:20 +0800

    first commit java file 1.0.1 begin to code

--- gmall-api/src/main/java/com/atguigu/gmall/bean/GitTest.java
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     // 初次下载代码进行开发
     public static void main(String[] args) {
         System.out.printf("dev/1.0.1...");
+
+        System.out.println("开始阿兹1.0.1 开发代码了");
     }
 }
