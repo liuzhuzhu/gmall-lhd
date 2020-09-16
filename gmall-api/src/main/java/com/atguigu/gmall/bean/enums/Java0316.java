565d1974e79500c4bdc1c7de4a4b91faf1e79090
liuzhuzhu <846061407@qq.com>
2020-09-16 14:33:31 +0800

    team1 在15的完成addcart功能

--- gmall-api/src/main/java/com/atguigu/gmall/bean/enums/Java0316.java
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/enums/Java0316.java
@@ -1,4 +1,14 @@
 package com.atguigu.gmall.bean.enums;
 
 public class Java0316 {
+    /**
+     * 2020 09 15
+     * team1 今天的任务已经完成 准备下班  下班之前需要将当天代码提交到team1 发分支上不需要合并
+     * 提交流程 git add
+     *      *          git commit
+     *      *              git push
+     */
+    public void addCart(){
+        System.out.println("添加购物车");
+    }
 }
