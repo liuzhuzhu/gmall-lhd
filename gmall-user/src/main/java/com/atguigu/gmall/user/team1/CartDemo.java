4b36eab0a6fc410a4531d34dc0946887f4518358
liuzhuzhu <846061407@qq.com>
2020-11-25 19:55:14 +0800

    team1 开发购物车  第一天

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/team1/CartDemo.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.user.team1;
+
+/**
+ * team1 开发cart
+ */
+public class CartDemo {
+    //第一天
+    public void addCart(){
+        System.out.println("team1 第一天 开发 购物车");
+    }
+}
